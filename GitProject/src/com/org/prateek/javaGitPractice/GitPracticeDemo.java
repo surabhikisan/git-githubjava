aa6ffcc0c715598c61c0a0eeb39d9075b18a66ee
prateek <hadoopdevprateek@gmail.com>
2019-04-28 11:29:00 +0530

    commited hello git project

---
+++ GitProject/src/com/org/prateek/javaGitPractice/GitPracticeDemo.java
@@ -0,0 +1,10 @@
+package com.org.prateek.javaGitPractice;
+
+public class GitPracticeDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello git bro by prateek");
+	}
+
+}
